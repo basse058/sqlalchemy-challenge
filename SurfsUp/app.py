be00f712a503074ae4e9fd1458b71ac554181b78
basse058 <basse058@gmail.com>
2022-12-10 20:08:13 -0600

    climate start complete, app.py in progress

---
+++ SurfsUp/app.py
@@ -0,0 +1,25 @@
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+# Database Setup
+database_path = "Resources/hawaii.sqlite"
+engine = create_engine(f"sqlite:///{database_path}")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+app = Flask(__name__)
+
+# Flask Routes
